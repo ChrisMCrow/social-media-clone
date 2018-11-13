cfb8115a1e5dffc7f019ac0c1c93927ab4923c24
Chan Lee and Chris Crow <chan+chris@>
2018-11-13 13:30:29 -0800

    setup project

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from 'reactstrap';
+//import PropTypes from "prop-types";
+
+function App(){
+  var styles = {
+  }
+  return (
+    <div className="jumbotron">
+      <h1>social-clone</h1>
+      <Button color="danger">Test</Button>
+    </div>
+  );
+}
+
+//App.propTypes = {
+//};
+
+export default App;
